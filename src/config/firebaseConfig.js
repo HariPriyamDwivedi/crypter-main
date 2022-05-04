607e6bf214b173df88bc4e6a7e02dad188daad67
HariPriyamDwivedi <82646916+HariPriyamDwivedi@users.noreply.github.com>
2022-05-04 18:20:56 +0530

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,34 @@
+/* const firebaseConfig = {
+    apiKey: "AIzaSyAYMPC_v5omND59qRiSyHb6MX9a6mZNF7k",
+    authDomain: "crypter-559dc.firebaseapp.com",
+    projectId: "crypter-559dc",
+    storageBucket: "crypter-559dc.appspot.com",
+    messagingSenderId: "70327679259",
+    appId: "1:70327679259:web:4c68cb139fc7be1436fbbe"
+  };
+
+  export default firebaseConfig; */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDotIGiaaQiqgZRW94RujLkcSxOfeZGE-g",
+  authDomain: "crypter-1bb27.firebaseapp.com",
+  projectId: "crypter-1bb27",
+  storageBucket: "crypter-1bb27.appspot.com",
+  messagingSenderId: "526777846008",
+  appId: "1:526777846008:web:b894ddc347473bd196130d",
+  measurementId: "G-KTSM6YVQLM"
+};
+
+// Initialize Firebase
+/* const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); */
+
+export default firebaseConfig;
